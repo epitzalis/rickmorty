874e4e81d80c72f3ec902a1936eb4fd7497b9484
HIBERUS\epitzalis <efisio.pitzalis@externos.w2m.com>
2022-12-06 19:15:20 +0100

    style improvements and keys translation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CharacterSearch } from '@models/character-search.model';
 import { RickMortyApiService } from '@services/rick-morty-api.service';
+import { Character } from '@models/character.model';
 
 @Component({
   selector: 'app-home',
@@ -10,7 +11,9 @@ import { RickMortyApiService } from '@services/rick-morty-api.service';
 })
 export class HomeComponent implements OnInit {
 
-  public characters: any[]
+  public characters: Character[];
+
+  public isLoading: boolean;
 
   constructor(
     private readonly rickMortyApiService: RickMortyApiService,
@@ -19,21 +22,24 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.activateRoute.queryParams.subscribe((resp: CharacterSearch) => {
-      this.findCharacters(resp)
+      this.findCharacters(resp);
     })
   }
 
   private findCharacters(params: CharacterSearch): void {
+    this.isLoading = true;
     this.rickMortyApiService.findCharacters(params).subscribe(
       {
         next: resp => {
           this.characters = resp
+          this.isLoading = false;
         },
         error: () => {
           this.characters = []
+          this.isLoading = false;
         }
       }
-    )
+    );
   }
 
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home.routing';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { FiltersModule } from '@components/filters/filters.module';
 import { CardModule } from '@components/card/card.module';
 import { TranslateModule } from '@ngx-translate/core';
@@ -16,6 +17,7 @@ import { TranslateModule } from '@ngx-translate/core';
     FiltersModule,
     CardModule,
     TranslateModule,
+    MatProgressSpinnerModule,
   ],
   exports: [
     HomeComponent
