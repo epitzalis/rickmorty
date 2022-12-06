e5836aa7c82f7699b5ab202b8f129136e93c8652
HIBERUS\epitzalis <efisio.pitzalis@externos.w2m.com>
2022-12-06 10:42:01 +0100

    add routing with lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module')
+      .then(m => m.HomeModule),
+  },
+  {
+    path: 'error-404',
+    loadChildren: () => import('./pages/error404/error404.module')
+      .then(m => m.Error404Module),
+  },
+  {
+    path: '**',
+    redirectTo: 'error-404',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/error404/error404.module.ts
+++ src/app/pages/error404/error404.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Error404Component } from './error404.component';
+import { Error404RoutingModule } from './error404.routing';
 
 @NgModule({
   declarations: [
     Error404Component
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    Error404RoutingModule
   ],
   exports: [
     Error404Component
